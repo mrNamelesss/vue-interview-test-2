80e95b06439c303ca6d4775ee298775503f8ebf7
mr_nameless <a.z.b@inbox.ru>
2018-04-04 17:36:55 +0300

    If profile Id is not specified, propose visit profile 18

--- src/views/Home/AssetHealth/TorqueProfile/TorqueProfile.vue
+++ src/views/Home/AssetHealth/TorqueProfile/TorqueProfile.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="torque-profile-view">
-    <div v-if="!loading">
-      <h3>Open</h3>
-      <p>id = {{ id }}</p>
-      <OpenTorque />
-      <h3>Close</h3>
-      <OpenTorque />
-    </div>
-    <div v-else>
-      <p>Loading...</p>
-    </div>
+    <template v-if="!id">
+      <router-link to="18">Go to profile 18</router-link>
+    </template>  
+    <template v-else>
+      <div v-if="!loading">
+        <h3>Open</h3>
+        <p>id = {{ id }}</p>
+        <OpenTorque />
+        <h3>Close</h3>
+        <OpenTorque />
+      </div>
+      <div v-else>
+        <p>Loading...</p>
+      </div>
+    </template>
   </div>
 </template>
 
