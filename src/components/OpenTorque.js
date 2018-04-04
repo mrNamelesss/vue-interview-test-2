0b2cc17056ca3215ea97012da54e4e7c059f92c5
mr_nameless <a.z.b@inbox.ru>
2018-04-04 17:03:58 +0300

    vars renamed

--- src/components/OpenTorque.js
+++ src/components/OpenTorque.js
@@ -5,21 +5,21 @@ export default {
   name: 'OpenTorqueChart',
   extends: Bar,
   mounted() {
-    const openTorqueData = TorqueProfileResource.getOpenTorqueData(18)
-    const openTorqueAverage = openTorqueData.map((it) => it['AverageTorque'])
-    const openTorqueLast = openTorqueData.map((it) => it['LastTorque'])
+    const torqueData = TorqueProfileResource.getOpenTorqueData(18)
+    const torqueAverage = torqueData.map((it) => it['AverageTorque'])
+    const torqueLast = torqueData.map((it) => it['LastTorque'])
     this.renderChart({
       labels: new Array(101).fill(0).map((_, i) => i), // [1..100]
       datasets: [
         {
           label: 'Average open torque',
           backgroundColor: '#4385f4',
-          data: openTorqueAverage
+          data: torqueAverage
         },
         {
           label: 'Last open torque',
           backgroundColor: '#6aa0fb',
-          data: openTorqueLast
+          data: torqueLast
         }
       ]
     }, {
