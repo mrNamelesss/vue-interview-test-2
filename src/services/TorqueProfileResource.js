9dbf9dc3b322923a84f1a0868b04bbf87fff12e7
mr_nameless <a.z.b@inbox.ru>
2018-04-04 17:07:42 +0300

    Refactor a little

--- src/services/TorqueProfileResource.js
+++ src/services/TorqueProfileResource.js
@@ -3,16 +3,14 @@ import mock from '@/services/mock'
 
 const TorqueProfileResource = {
   getOpenTorqueData(profileId) {
-    // profileId === 18
     return mock.TorqueData
-      .filter((item) => item['Direction'] === "Open")
       .filter((item) => item['Profile'] === profileId)
+      .filter((item) => item['Direction'] === "Open")
   },
   getCloseTorqueData(profileId) {
-    // profileId === 18
     return mock.TorqueData
-      .filter((item) => item['Direction'] === "Close")
       .filter((item) => item['Profile'] === profileId)
+      .filter((item) => item['Direction'] === "Close")
   }
 }
 
